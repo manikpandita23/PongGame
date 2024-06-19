9bbffd6c05b96bb544b454c8e56e436577371b98
manikpandita23 <115569552+manikpandita23@users.noreply.github.com>
2024-06-19 23:21:59 +0530

    Checking borders

--- pong.py
+++ pong.py
@@ -73,3 +73,26 @@ while True:
 
     hit_ball.setx(hit_ball.xcor()+hit_ball.dx)
     hit_ball.sety(hit_ball.ycor()+hit_ball.dy)
+
+    if hit_ball.ycor()>200:
+        hit_ball.sety(280)
+        hit_ball.dy *= -1
+    
+    if hit_ball.ycor()<-280:
+        hit_ball.sety(-280)
+        hit_ball.dy *= -1
+    
+    if hit_ball.xcor()>500:
+        hit_ball.goto(0,0)
+        hit_ball.dy *= -1
+        left_player+=1
+        sketch.clear()
+        sketch.write("Left_player : {}    Right_player: {}".format(left_player, right_player),align="center",font=("Courier",24,"normal"))
+
+    if hit_ball.xcor()<-500:
+        hit_ball.goto(0,0)
+        hit_ball.dy *= -1
+        right_player+=1
+        sketch.clear()
+        sketch.write("Left_player : {}    Right_player: {}".format(left_player, right_player), align="center",font=("Courier", 24, "normal"))
+        
