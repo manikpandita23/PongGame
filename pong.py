b5cd1d718ab43f88e3a0bc6ffedb3a951eae6da7
manikpandita23 <115569552+manikpandita23@users.noreply.github.com>
2024-06-19 22:47:24 +0530

    Ball shape

--- pong.py
+++ pong.py
@@ -20,3 +20,13 @@ right_pad.color("black")
 right_pad.shapesize(stretch_wid=6, stretch_len=2)
 right_pad.penup()
 right_pad.goto(400, 0)
+
+hit_ball = turtle.Turtle()
+hit_ball.speed(40)
+hit_ball.shape("circle")
+hit_ball.color("red")
+hit_ball.penup()
+hit_ball.goto(0, 0)
+hit_ball.dx = 5
+hit_ball.dy = -5
+
